cb8c296bb4655345f01d46f4e264cf6bc54e7bbf
Leonardo Martins Souza <leonardo.msouza@hotmail.com>
2021-03-15 02:42:32 -0300

    add gitignore e criando a Nav-Bar com as tabelas de crud

--- DjangoFront/settings.py
+++ DjangoFront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from secret import django_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rrgo=$g5i7)@hfv!7ib950s7)97*j&u1qwk&f9%4^zl&4@uk^%'
+SECRET_KEY = django_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
