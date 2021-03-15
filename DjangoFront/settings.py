55287ae1eaaf747e5740e19530cbf76e12a4a95a
Leonardo Martins Souza <leonardo.msouza@hotmail.com>
2021-03-15 03:53:14 -0300

    adicionando mais css nas tabelas e navbar

--- DjangoFront/settings.py
+++ DjangoFront/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from secret import django_key
+from secret import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'TFG',
         'USER': 'postgres',
-        'PASSWORD': '0812',
+        'PASSWORD': postgre_key,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
